debfc31eff9f0f4db879045a73d9d90a7daa3c0c
rohanvachhani <rohan.vachhani5@gmail.com>
2017-12-24 23:23:25 +0530

    final

--- app/src/main/java/com/rohan/test/AboutUs.java
+++ app/src/main/java/com/rohan/test/AboutUs.java
@@ -3,16 +3,11 @@ package com.rohan.test;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class AboutUs extends WithMenuActivity {
 
-    Animation animation;
-    TextView textView;
 
     //for differnt font
     protected void attachBaseContext(Context newBase) {
@@ -31,10 +26,6 @@ public class AboutUs extends WithMenuActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboout_us);
 
-        animation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.fade_in);
 
-        textView = findViewById(R.id.text_v);
-        textView.startAnimation(animation);
     }
 }
